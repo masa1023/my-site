7af78d51b3b2ea1af6a5da9154fa62d52c589baa
Masa <masa373.dev@gmail.com>
2025-04-13 01:19:46 +0900

    feat: add OG image generation for blog posts

---
+++ app/blog/[slug]/opengraph-image.tsx
@@ -0,0 +1,41 @@
+import { getPostBySlug } from '@/lib/posts'
+import { notFound } from 'next/navigation'
+import { ImageResponse } from 'next/og'
+
+export const size = {
+  width: 1200,
+  height: 630,
+}
+
+export const contentType = 'image/png'
+
+export default async function Image({ params }: { params: { slug: string } }) {
+  const post = await getPostBySlug(params.slug)
+
+  if (!post) {
+    notFound()
+  }
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          height: '100%',
+          width: '100%',
+          textAlign: 'center',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: '#fff',
+          padding: '32px',
+        }}
+      >
+        <div style={{ marginTop: 40, fontSize: 56, fontWeight: 600 }}>
+          {post.title}
+        </div>
+        <p>masa373.work</p>
+      </div>
+    )
+  )
+}
