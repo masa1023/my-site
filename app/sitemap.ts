b506e512fc66b8d31b12f7119effa67afdecedaa
Masa <masa373.dev@gmail.com>
2026-04-07 10:49:32 +0900

    fix: sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,26 +1,26 @@
-import { getAllPosts, Post } from '@/lib/posts'
+import { getAllPosts } from '@/lib/posts'
 import type { MetadataRoute } from 'next'
 
 export const dynamic = 'force-static'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const posts = getAllPosts()
-  const lastPost = posts[0]
-  const firstPost = posts[posts.length - 1]
+  const latestDate = posts[0]?.date
 
   return [
     {
       url: 'https://masa373.work',
-      lastModified: lastPost.date,
+      lastModified: latestDate,
       changeFrequency: 'monthly',
     },
     {
       url: 'https://masa373.work/blog',
-      lastModified: firstPost.date,
+      lastModified: latestDate,
       changeFrequency: 'weekly',
     },
-    ...posts.map((post: Post) => ({
+    ...posts.map((post) => ({
       url: `https://masa373.work/blog/${post.slug}`,
+      lastModified: post.date,
       changeFrequency: 'monthly' as const,
     })),
   ]
